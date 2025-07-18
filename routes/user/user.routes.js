6fda5d83e41e3fec86920f822f8f42cf3d030803
Pratik Dhamecha <dhamechapratik5@gmail.com>
2025-07-18 18:46:07 +0530

    Removed Grid FS

--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const UserController = require('../../controllers/user/user.controller');
 const router = express.Router();
 const verifyToken = require('../../middleware/middleware');
-const upload = require('../../Config/multer');
 
 
 // Auth routes
@@ -12,7 +11,6 @@ router.post('/login', UserController.loginUser);
 // Profile routes
 router.get('/profile/:userId', verifyToken, UserController.getUserProfile);
 router.put('/profile/:userId', verifyToken, UserController.updateUserProfile);
-router.put('/profile/upload/:userId',upload.single('image'), UserController.updateUserProfile);
 // XP and stats routes
 router.post('/xp/:userId', verifyToken, UserController.addXP);
 router.post('/stats/:userId', verifyToken, UserController.updateUserStats);
